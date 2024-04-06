950ca6f1506e38e733682d2bd1229e3491781f93
Lenovo <Lenovo@DESKTOP-NKOLHH0>
2024-04-06 21:08:39 +0530

    This is Customer Service for Shopping mall.

---
+++ CustomerService_DhivyaDharshiniN/src/main/java/com/tnsif/customerservice/CustomerServiceDhivyaDharshiniNApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceDhivyaDharshiniNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceDhivyaDharshiniNApplication.class, args);
+	}
+
+}
---
+++ CustomerService_DhivyaDharshiniN/src/test/java/com/tnsif/customerservice/CustomerServiceDhivyaDharshiniNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceDhivyaDharshiniNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
